546286a784ddb871e025b0e71202623c7f8f973e
Timothy Wu <timothywu02@gmail.com>
2019-11-09 17:04:16 -0500

    Shooter

--- robot/src/main/java/us/ilite/robot/modules/Shooter.java
+++ robot/src/main/java/us/ilite/robot/modules/Shooter.java
@@ -1,4 +1,53 @@
 package us.ilite.robot.modules;
 
-public class Shooter {
+import com.revrobotics.CANSparkMax;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import us.ilite.robot.hardware.NeoDriveHardware;
+
+public class Shooter extends Module
+{
+    private CANSparkMax mMotor;
+    private boolean spinning;
+    public static final double SHOOTER_MAX_VELOCITY = 1.0;
+    public Shooter( CANSparkMax mMotor, Joystick mActivate, Joystick mDeactivate )
+    {
+        this.mMotor = mMotor;
+    }
+    @Override
+    public void modeInit(double pNow)
+    {
+
+    }
+
+    @Override
+    public void periodicInput(double pNow)
+    {
+
+    }
+
+    @Override
+    public void update(double pNow)
+    {
+        if ( spinning )
+        {
+            mMotor.set( SHOOTER_MAX_VELOCITY );
+        }
+    }
+
+    @Override
+    public void shutdown(double pNow)
+    {
+
+    }
+
+    public void activate()
+    {
+        spinning = true;
+    }
+
+    public void deactivate()
+    {
+        spinning = false;
+    }
 }
