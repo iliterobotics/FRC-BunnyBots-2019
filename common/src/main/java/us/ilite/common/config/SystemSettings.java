f3259cf83c0cdd51c7f9795f528348f58d52c594
Gleb Babahov <gl.babahov@gmail.com>
2019-11-11 15:12:28 -0500

    Initial module commit

--- common/src/main/java/us/ilite/common/config/SystemSettings.java
+++ common/src/main/java/us/ilite/common/config/SystemSettings.java
@@ -134,7 +134,7 @@ public class SystemSettings extends NetworkTablesConstantsBase {
 
     // =============================================================================
     // LimeLight Camera Constants
-    // Note: These constants need to be recalculted for a specific robot geometry
+    // Note: These constants need to be recalculated for a specific robot geometry
     // =============================================================================
     public static double llCameraHeightIn = 58.0;
     public static double llCameraToBumperIn = 10.0;
---
+++ robot/src/main/java/us/ilite/robot/modules/Catapult.java
@@ -0,0 +1,28 @@
+package us.ilite.robot.modules;
+
+import edu.wpi.first.wpilibj.Servo;
+
+public class Catapult extends Module {
+
+    private Servo mServo;
+    
+    @Override
+    public void modeInit(double pNow) {
+
+    }
+
+    @Override
+    public void periodicInput(double pNow) {
+
+    }
+
+    @Override
+    public void update(double pNow) {
+        
+    }
+
+    @Override
+    public void shutdown(double pNow) {
+
+    }
+}
