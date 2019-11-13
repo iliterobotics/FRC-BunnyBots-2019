0f9f9d273d1085b62811d41d8920e9875f4f09c4
John Anselmo <jjohnanselmo@gmail.com>
2019-11-12 22:49:12 -0500

    Robot dimensions

--- robot/src/main/java/us/ilite/robot/auto/paths/RobotDimensions.java
+++ robot/src/main/java/us/ilite/robot/auto/paths/RobotDimensions.java
@@ -4,6 +4,8 @@ package us.ilite.robot.auto.paths;
  * All units in inches.
  */
 public class RobotDimensions {
-
+    public final double kSideToCenter = 31.5 / 2;
+    public final double kBackToCenter = 28.0 / 2;
+    public final double kFrontToCenter = kBackToCenter;
 
 }
