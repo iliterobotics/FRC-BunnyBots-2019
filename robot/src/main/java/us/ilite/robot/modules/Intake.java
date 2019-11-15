6aa7d92bbb8a934c2cb5d6cb964a56d73321d90c
Faris Syed <farisssyed25@gmail.com>
2019-11-14 20:12:25 -0500

    Intake Class Basic Structure
    - Created Enum for states of Intake
    - Programmed Talon

--- robot/src/main/java/us/ilite/robot/modules/Intake.java
+++ robot/src/main/java/us/ilite/robot/modules/Intake.java
@@ -2,51 +2,59 @@ package us.ilite.robot.modules;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.flybotix.hfr.util.log.ILog;
 import com.flybotix.hfr.util.log.Logger;
-import com.team254.lib.drivers.talon.TalonSRXFactory;
 
-import edu.wpi.first.wpilibj.Solenoid;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import us.ilite.common.Data;
+//import us.ilite.common.Data;
 import us.ilite.common.config.SystemSettings;
-//import us.ilite.common.types.manipulator.EIntake;
-import us.ilite.common.types.sensor.EPowerDistPanel;
-//import us.ilite.robot.driverinput.DriverInput.EGamePiece;
-import us.ilite.robot.hardware.SolenoidWrapper;
+
 
 public class Intake extends Module {
 
-    private final double kZero = 0.0;
-    private final String kDefaultError = "This message should not be displayed";
-
     private ILog mLog = Logger.createLog(Intake.class);
-    private Data mData;
+    private TalonSRX mIntakeRoller;
+    private double mDesiredPower;
+    private EIntakeState mDesiredIntakeState;
 
-    private VictorSPX mIntakeRoller;
-    private double mIntakeRollerCurrent;
-    private double mIntakeRollerVoltage;
-
-    private TalonSRX mTalon;
+    public Intake() {
+        mIntakeRoller = new TalonSRX(SystemSettings.kIntakeId);
+        mDesiredPower = 0;
+    }
 
     @Override
     public void modeInit(double pNow) {
-
+        mLog.error("MODE INIT");
     }
 
     @Override
     public void periodicInput(double pNow) {
-
+        //TODO
     }
 
     @Override
     public void update(double pNow) {
-
+        switch (mDesiredIntakeState) {
+            case INTAKING:
+                mIntakeRoller.set(ControlMode.PercentOutput, mDesiredPower);
+                break;
+            case OUTTAKING:
+                mIntakeRoller.set(ControlMode.PercentOutput, -mDesiredPower);
+                break;
+            default:
+                mIntakeRoller.set(ControlMode.PercentOutput, 0);
+                break;
+        }
     }
 
     @Override
     public void shutdown(double pNow) {
-
+        mDesiredPower = 0d;
     }
+
+    private enum EIntakeState {
+        INTAKING,
+        OUTTAKING;
+    }
+
+
 }
