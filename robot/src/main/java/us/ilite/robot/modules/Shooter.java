279739284489e9d5f1d66730afa1f05fbdf56921
John Anselmo <jjohnanselmo@gmail.com>
2019-11-16 14:36:48 -0500

    Change to case and enum

--- robot/src/main/java/us/ilite/robot/modules/Shooter.java
+++ robot/src/main/java/us/ilite/robot/modules/Shooter.java
@@ -8,9 +8,17 @@ import us.ilite.common.lib.control.PIDController;
 public class Shooter extends Module {
     private Talon mShooterLeft;
     private Talon mShooterRight;
+    private EShooterState mShooterState;
     private static boolean spinning;
+    private double mDesiredOutput;
 
     private PIDController kShooterPidController = new PIDController(SystemSettings.kShooterGains, 0, SystemSettings.kMaxShooter, SystemSettings.kControlLoopPeriod );
+
+    private enum EShooterState {
+        SHOOTING,
+        GIVE_TO_HOPPER,
+        STOP;
+    }
 
     public Shooter() {
         kShooterPidController.setOutputRange( 0, 1 );
@@ -39,15 +47,21 @@ public class Shooter extends Module {
 
     @Override
     public void update(double pNow) {
-        if ( spinning ) {
-            mShooterLeft.set( kShooterPidController.calculate( mShooterLeft.getSpeed(), pNow ) );
-            mShooterRight.set( kShooterPidController.calculate( mShooterRight.getSpeed(), pNow ) );
-        }
-        else {
-            mShooterLeft.set( kShooterPidController.calculate( mShooterLeft.getSpeed(), 0 ) );
-            mShooterRight.set( kShooterPidController.calculate( mShooterRight.getSpeed(), 0 ) );
+        switch (mShooterState) {
+            case SHOOTING:
+                mDesiredOutput = kShooterPidController.calculate(mShooterLeft.getSpeed(), pNow);
+            case GIVE_TO_HOPPER:
+                mDesiredOutput = -1.0;
+            case STOP:
+                mDesiredOutput = 0.0;
         }
 
+        mShooterLeft.set(mDesiredOutput);
+        mShooterRight.set(-mDesiredOutput);
+    }
+
+    public void setShooterState(EShooterState pState) {
+        mShooterState = pState;
     }
 
     @Override
