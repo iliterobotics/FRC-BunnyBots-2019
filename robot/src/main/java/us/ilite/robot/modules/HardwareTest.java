97bcf7bdc9c210b185d1600deaadcff862fa13e1
Agrim Sharma <75flyershd@gmail.com>
2019-11-27 11:49:20 -0500

    Enum added for Hardware

--- robot/src/main/java/us/ilite/robot/modules/HardwareTest.java
+++ robot/src/main/java/us/ilite/robot/modules/HardwareTest.java
@@ -4,17 +4,25 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
 import us.ilite.lib.drivers.SparkMaxFactory;
 
-import java.net.SocketAddress;
-
 public class HardwareTest extends Module {
     private CANSparkMax mCANSparkMax1;
     private CANSparkMax mCANSparkMax2;
+    private EHardwareState mHardwareState;
+
+    public enum EHardwareState {
+        NORMAL(1.0),
+        INVERSE(-1.0),
+        STOP(0.0);
+        private double power;
+        EHardwareState(double pow){
+            power = pow;
+        }
+    }
 
 
     public HardwareTest() {
         mCANSparkMax1 = SparkMaxFactory.createDefaultSparkMax(2, CANSparkMaxLowLevel.MotorType.kBrushless);
         mCANSparkMax2 = SparkMaxFactory.createDefaultSparkMax(3, CANSparkMaxLowLevel.MotorType.kBrushless);
-
         mCANSparkMax1.setInverted(true);
     }
 
