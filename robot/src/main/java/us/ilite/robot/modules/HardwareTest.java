eeee6ffb5e8a0e81dea23e7d82a6c457fed82d7e
Ibrahim Abbasi <Ibrahimzabbasi04@gmail.com>
2019-12-03 17:14:17 -0800

    Actual commits with slaves

--- robot/src/main/java/us/ilite/robot/modules/HardwareTest.java
+++ robot/src/main/java/us/ilite/robot/modules/HardwareTest.java
@@ -24,17 +24,19 @@ public class HardwareTest extends Module {
 
     public HardwareTest() {
         mCANSparkMax1 = SparkMaxFactory.createDefaultSparkMax(6, CANSparkMaxLowLevel.MotorType.kBrushless);
-        mCANSparkMax2 = SparkMaxFactory.createDefaultSparkMax(7, CANSparkMaxLowLevel.MotorType.kBrushless);
+        mCANSparkMax2 = SparkMaxFactory.createPermanentSlaveSparkMax(7, mCANSparkMax1, CANSparkMaxLowLevel.MotorType.kBrushless);
+
         mCANSparkMax3 = SparkMaxFactory.createDefaultSparkMax(4, CANSparkMaxLowLevel.MotorType.kBrushless);
-        mCANSparkMax4 = SparkMaxFactory.createDefaultSparkMax(5, CANSparkMaxLowLevel.MotorType.kBrushless);
+        mCANSparkMax4 = SparkMaxFactory.createPermanentSlaveSparkMax(5, mCANSparkMax3, CANSparkMaxLowLevel.MotorType.kBrushless);
 
         mCANSparkMax1.setInverted(true);
-        mCANSparkMax3.setInverted(true);
+//        mCANSparkMax3.setInverted(true);
     }
 
     @Override
     public void modeInit(double pNow) {
-
+        mCANSparkMax1.setIdleMode(CANSparkMax.IdleMode.kBrake);
+        mCANSparkMax3.setIdleMode(CANSparkMax.IdleMode.kBrake);
     }
 
     @Override
@@ -45,9 +47,8 @@ public class HardwareTest extends Module {
     @Override
     public void update(double pNow) {
         mCANSparkMax1.set(1.0);
-        mCANSparkMax2.set(1.0);
         mCANSparkMax3.set(1.0);
-        mCANSparkMax4.set(1.0);
+
     }
 
     @Override
