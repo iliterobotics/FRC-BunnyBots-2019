a8bd9bcdf7fb63fdd42c9d60e72dc7ae55e26a1c
Agrim Sharma <75flyershd@gmail.com>
2019-12-05 20:59:55 -0500

    ready for testing

--- robot/src/main/java/us/ilite/robot/commands/LimelightTargetLock.java
+++ robot/src/main/java/us/ilite/robot/commands/LimelightTargetLock.java
@@ -2,6 +2,8 @@ package us.ilite.robot.commands;
 
 import com.flybotix.hfr.util.log.ILog;
 import com.flybotix.hfr.util.log.Logger;
+import us.ilite.common.config.SystemSettings;
+import us.ilite.common.lib.control.PIDController;
 import us.ilite.common.types.ETrackingType;
 import us.ilite.robot.modules.Drive;
 import us.ilite.robot.modules.IThrottleProvider;
@@ -23,7 +25,6 @@ public class LimelightTargetLock extends TargetLock {
 
     public LimelightTargetLock(Drive pDrive, Limelight pLimelight, double pAllowableError, ETrackingType pTrackingType, IThrottleProvider pThrottleProvider, boolean pEndOnAlignment) {
         super(pDrive, pAllowableError, pTrackingType, pLimelight, pThrottleProvider, pEndOnAlignment);
-
         this.mLimelight = pLimelight;
         this.mTrackingType = pTrackingType;
     }
@@ -33,6 +34,9 @@ public class LimelightTargetLock extends TargetLock {
         super.init(pNow);
         mLimelight.setTracking(mTrackingType);
         mLog.error("STARTED LIMELIGHT TARGET LOCK");
+        mLimelight.setVisionTarget(SystemSettings.VisionTarget.High);
+
+
     }
 
     public void shutdown(double pNow) {
