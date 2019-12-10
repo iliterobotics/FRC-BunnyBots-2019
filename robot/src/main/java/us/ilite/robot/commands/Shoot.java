b999648c167fd46fb1dde365caeb610e8583ca8b
John Anselmo <jjohnanselmo@gmail.com>
2019-12-10 14:29:58 -0500

    Added shoot command and boolean for ability to shoot until current drop

--- robot/src/main/java/us/ilite/robot/commands/Shoot.java
+++ robot/src/main/java/us/ilite/robot/commands/Shoot.java
@@ -1,9 +1,20 @@
 package us.ilite.robot.commands;
 
+import us.ilite.robot.modules.Shooter;
+
 public class Shoot implements ICommand {
+
+    private Shooter mShooter;
+    private boolean mShootUntilEmpty;
+
+    public Shoot(Shooter pShooter, boolean pShootUntilEmpty) {
+        mShooter = pShooter;
+        mShootUntilEmpty = pShootUntilEmpty;
+    }
+
     @Override
     public void init(double pNow) {
-        
+        mShooter.setShooterState(Shooter.EShooterState.SHOOTING);
     }
 
     @Override
