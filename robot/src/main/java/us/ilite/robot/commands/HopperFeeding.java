c9c7a7967dea3d82e295aec4f2c66ff6a71d8395
John Anselmo <jjohnanselmo@gmail.com>
2019-12-10 19:36:06 -0500

    Give to shooter command for auto

---
+++ robot/src/main/java/us/ilite/robot/commands/HopperFeeding.java
@@ -0,0 +1,26 @@
+package us.ilite.robot.commands;
+
+import us.ilite.robot.modules.Hopper;
+
+public class HopperFeeding implements ICommand {
+
+    private Hopper mHopper;
+
+    public HopperFeeding(Hopper pHopper) {
+        mHopper = pHopper;
+    }
+    @Override
+    public void init(double pNow) {
+        mHopper.setHopperState(Hopper.EHopperState.GIVE_TO_SHOOTER);
+    }
+
+    @Override
+    public boolean update(double pNow) {
+        return false;
+    }
+
+    @Override
+    public void shutdown(double pNow) {
+
+    }
+}
