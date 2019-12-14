db95da805b6f7720b63fa237b97cfe3e751f1fe3
Daniel Tolbert <danielchristopher19@gmail.com>
2019-12-14 18:00:20 -0500

    attempting shoot command

--- robot/src/main/java/us/ilite/robot/commands/ShootCommand.java
+++ robot/src/main/java/us/ilite/robot/commands/ShootCommand.java
@@ -1,9 +1,17 @@
 package us.ilite.robot.commands;
 
+import com.flybotix.hfr.util.log.ILog;
+import com.flybotix.hfr.util.log.Logger;
+import us.ilite.robot.modules.Drive;
 import us.ilite.robot.modules.Shooter;
 
 public class ShootCommand implements ICommand {
     private Shooter mShooter;
+    public ILog mLog = Logger.createLog(ShootCommand.class);
+    private double mDelay;
+    private Drive mDrive;
+
+    MoveForNCycles mMoveForNCycles = new MoveForNCycles( 1.0 , 1.0 ,  7 , false , 2 , mDrive);
 
     public ShootCommand(Shooter pShooter){
         this.mShooter = pShooter;
@@ -11,16 +19,26 @@ public class ShootCommand implements ICommand {
 
     @Override
     public void init(double pNow) {
-
+        mShooter.setShooterState(Shooter.EShooterState.SHOOTING);
     }
 
     @Override
     public boolean update(double pNow) {
+        mLog.info("-------------------Working----------------------");
+        if ( mMoveForNCycles.update(pNow) ){
+
+        }
         return false;
+
     }
 
     @Override
     public void shutdown(double pNow) {
         mShooter.shutdown(pNow);
     }
+
+    public void createDelay(){
+
+    }
+
 }
