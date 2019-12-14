9be88aa020001fb118723f279e9b1937afa3dd41
Daniel Tolbert <danielchristopher19@gmail.com>
2019-12-14 15:59:24 -0500

    start UNUSED trajectory

--- robot/src/main/java/us/ilite/robot/auto/paths/ShootBunnyShootCrossNeutralLine.java
+++ robot/src/main/java/us/ilite/robot/auto/paths/ShootBunnyShootCrossNeutralLine.java
@@ -1,4 +1,16 @@
 package us.ilite.robot.auto.paths;
 
-public class ShootBunnyShootCrossNeutralLine {
+import us.ilite.common.lib.trajectory.TrajectoryGenerator;
+import us.ilite.robot.commands.ICommand;
+
+public class ShootBunnyShootCrossNeutralLine extends AutoSequence{
+
+    public ShootBunnyShootCrossNeutralLine(TrajectoryGenerator pTrajectoryGenerator) {
+        super(pTrajectoryGenerator);
+    }
+
+    @Override
+    public ICommand[] generateSequence() {
+        return new ICommand[0];
+    }
 }
