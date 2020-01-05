6cd718abc7dafe426b317ea5752d357062512d11
Agrim Sharma <75flyershd@gmail.com>
2020-01-05 14:12:33 -0500

    Logging added

--- robot/src/main/java/us/ilite/robot/auto/paths/TestTrajectory.java
+++ robot/src/main/java/us/ilite/robot/auto/paths/TestTrajectory.java
@@ -1,5 +1,8 @@
 package us.ilite.robot.auto.paths;
 
+import com.flybotix.hfr.util.log.ILog;
+import com.flybotix.hfr.util.log.Logger;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import us.ilite.common.lib.trajectory.TrajectoryGenerator;
 import us.ilite.common.types.ETrackingType;
 import us.ilite.robot.commands.*;
@@ -11,7 +14,9 @@ public class TestTrajectory extends AutoSequence {
   private Limelight mAimBot;
   private ETrackingType mTrackingType;
   private Hopper mHopper;
+  private DriveMessage mMessage;
   private IThrottleProvider mThrottleProvider;
+  private ILog mLogger  = Logger.createLog(TestTrajectory.class);
 
     public TestTrajectory(TrajectoryGenerator pTrajectoryGenerator , Drive pDrive , Shooter pShooter,
                           Limelight pAimBot , ETrackingType pTrackingType, Hopper pHopper , IThrottleProvider pThrottleProvider) {
@@ -26,6 +31,13 @@ public class TestTrajectory extends AutoSequence {
 
     @Override
     public ICommand[] generateSequence() {
+        mLogger.error("-------------------------------------------------------------------390287490128734------------------");
+
+        SmartDashboard.putNumber("Velocity" , mShooter.getCurrentVelocity());
+        SmartDashboard.putNumber("Left velocity" , mMessage.leftOutput );
+        SmartDashboard.putNumber("Right velocity" , mMessage.rightOutput );
+
+
         return new ICommand[] { new LimelightTargetLock( mDrive , mAimBot , 10 , mTrackingType , mThrottleProvider)  ,
                  new MoveForNCycles ( 0.5d , 0.5d , 2 , false , 10 , mDrive) ,
                 new ShootFlywheel(mShooter , mHopper) , new FeedToFlywheel(mHopper) , new MoveForNCycles (1d , 1d ,
